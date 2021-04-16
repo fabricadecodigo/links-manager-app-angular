b0e7c5f8da4d1396cab401e5f2ba0fecdf6e0e0b
Felipe <felipe@fabricadecodigo.com>
2021-04-16 09:11:12 -0300

    tela criar conta testes

--- src/app/onboarding/pages/create-account/create-account.component.spec.ts
+++ src/app/onboarding/pages/create-account/create-account.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from '@core/modules';
 import { CreateAccountComponent } from './create-account.component';
 
 describe('CreateAccountComponent', () => {
@@ -8,9 +10,9 @@ describe('CreateAccountComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateAccountComponent ]
-    })
-    .compileComponents();
+      declarations: [CreateAccountComponent],
+      imports: [NoopAnimationsModule, ReactiveFormsModule, AngularMaterialModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
